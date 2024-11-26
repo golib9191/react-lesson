dd8c8bf94c790568b79a8e638de34b3cfd4b9022
golib9191 <octa.g91@gmail.com>
2024-11-26 21:44:49 +0500

    Header,Main,Footer

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header.jsx";
+import MainContent from "./components/MainContent.jsx";
+import Footer from "./components/Footer.jsx";
+
+export const App = () => {
+    return (
+        <div>
+            <Header/>
+            <MainContent/>
+            <Footer/>
+        </div>
+    )
+}
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+const MainContent = () => {
+    return (
+        <main>
+            <h2>Main Content</h2>
+            <p>Bu main content</p>
+        </main>
+    )
+}
+export default MainContent

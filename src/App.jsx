b2dde77d5c7149605ab7baa71b9a9c1c7def6249
golib9191 <octa.g91@gmail.com>
2024-11-26 21:52:24 +0500

    Header,Main,Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import Header from "./components/Header.jsx";
-import MainContent from "./components/MainContent.jsx";
-import Footer from "./components/Footer.jsx";
+import JSXRules from "./components/JSXRules.jsx";
 
 export const App = () => {
     return (
         <div>
-            <Header/>
-            <MainContent/>
-            <Footer/>
+            <JSXRules/>
         </div>
     )
 }
--- src/components/MainContent.jsx
+++
@@ -1,9 +0,0 @@
-const MainContent = () => {
-    return (
-        <main>
-            <h2>Main Content</h2>
-            <p>Bu main content</p>
-        </main>
-    )
-}
-export default MainContent

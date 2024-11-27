e143884c684ba92f3a913311dc794aa9bfd55d15
golib9191 <octa.g91@gmail.com>
2024-11-27 20:19:51 +0500

    Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import ProductList from "./components/ProductList.jsx";
+import Person from "./components/Person.jsx";
+
 
 export const App = () => {
     return (
         <div>
-            <ProductList/>
+            <Person name="G'olib" age={33} hobbie={"PUBG"} family={"Oilali 2 ta farzandli"} />
         </div>
     )
 }

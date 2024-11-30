9644bfe0722f5b25de79eb5eab167032a2f471d6
golib9191 <octa.g91@gmail.com>
2024-11-30 15:16:16 +0500

    copyRight da prompt surov

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import "./index.css"
-import { AiFillAlipayCircle } from "react-icons/ai";
-export const App = () => {
+const Copy = () => {
+    function copyRights(){
+        prompt('text kochirish mumkinmas')
+    }
+
     return (
-        <section>
-            <AiFillAlipayCircle size="xl" />
-        </section>
+        <p onCopy={copyRights}>
+            viris purus graeco omittam adolescens movet maximus oporteat viverra efficiantur instructior sententiae
+            posidonium appareat quam pertinax sonet suas maximus alienum penatibus numquam unum congue posidonium
+            molestiae urna consectetur salutatus quidam
+        </p>
     )
 }
 
+
+export const App = () => {
+    return (
+        <section>
+            <Copy/>
+        </section>
+    )
+}
